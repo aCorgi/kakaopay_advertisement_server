8c9757c857c7622f1ad8183361edf495eb69d45c
aCorgi <leehae2213@gmail.com>
2025-07-18 00:15:52 +0900

    feat: first init

---
+++ src/test/kotlin/com/task/kakaopayadvertisementserver/KakaopayAdvertisementServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.task.kakaopayadvertisementserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KakaopayAdvertisementServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
