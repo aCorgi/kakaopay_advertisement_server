468048d537daa4853a9c9a7f8e398db0e6552e27
aCorgi <leehae2213@gmail.com>
2025-07-18 23:12:35 +0900

    fix: post 로 생성 시 201 created 응답 status code 로 반환

--- src/main/kotlin/com/task/kakaopayadvertisementserver/controller/admin/AdvertisementController.kt
+++ src/main/kotlin/com/task/kakaopayadvertisementserver/controller/admin/AdvertisementController.kt
@@ -4,10 +4,12 @@ import com.task.kakaopayadvertisementserver.dto.AdvertisementCreationRequest
 import com.task.kakaopayadvertisementserver.service.AdvertisementService
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @Validated
@@ -18,6 +20,7 @@ class AdvertisementController(
     private val advertisementService: AdvertisementService,
 ) {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createAdvertisement(
         @RequestBody @Valid request: AdvertisementCreationRequest,
     ) {
