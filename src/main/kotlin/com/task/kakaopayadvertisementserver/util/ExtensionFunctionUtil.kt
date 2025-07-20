a455bd625c8f1ad1c0c6e5d4a501895c1b4b5e83
aCorgi <leehae2213@gmail.com>
2025-07-21 01:10:59 +0900

    refactor: 미사용 클래스, 함수 제거

--- src/main/kotlin/com/task/kakaopayadvertisementserver/util/ExtensionFunctionUtil.kt
+++ src/main/kotlin/com/task/kakaopayadvertisementserver/util/ExtensionFunctionUtil.kt
@@ -6,16 +6,3 @@ import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 
 inline fun <reified T> logger() = LoggerFactory.getLogger(T::class.java)
-
-fun <T> List<T>.paginate(pageable: Pageable): Page<T> {
-    val start = pageable.offset.toInt()
-    val end = minOf(start + pageable.pageSize, this.size)
-    val content =
-        if (start < this.size) {
-            this.subList(start, end)
-        } else {
-            emptyList()
-        }
-
-    return PageImpl(content, pageable, this.size.toLong())
-}
